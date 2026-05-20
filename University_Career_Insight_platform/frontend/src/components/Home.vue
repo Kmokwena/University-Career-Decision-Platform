7e46608c309b88c44819824ccbacdf1fd229e5cb
kmokwena <mokwenakw@gmail.com>
2026-05-20 09:04:30 +0200

    added front end

---
+++ University_Career_Insight_platform/frontend/src/components/Home.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>Welcome to the University Career Insight Platform</h1>
+    <p>
+      Discover your career path with our comprehensive insights and resources.
+    </p>
+    <button class="get-started">Get Started</button>
+  </main>
+</template>
